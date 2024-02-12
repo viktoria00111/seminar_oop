d13cbd0fb4dcd768a6f67369c6acc6ada21ac691
viktoria00111 <123251981+viktoria00111@users.noreply.github.com>
2024-02-12 22:25:19 +0300

    Update User.java

---
+++ S5/DZ5/User.java
@@ -0,0 +1,36 @@
+public class User {
+    private String firstName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    private String lastName;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    private String middleName;
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+    public User(String firstName, String lastName, String middleName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.middleName = middleName;
+    }
+}
