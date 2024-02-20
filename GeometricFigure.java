4199dbe1064995e6c5148061fd6f34d000ad921e
viktoria00111 <123251981+viktoria00111@users.noreply.github.com>
2024-02-20 12:04:24 +0300

    Update GeometricFigure.java

---
+++ GeometricFigure.java
@@ -0,0 +1,14 @@
+package work1;
+
+public class GeometricFigure {
+    int height;
+    int width;
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+}
