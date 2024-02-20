f1acd18b572b3eb5afd61fc3587d819c286f7847
viktoria00111 <123251981+viktoria00111@users.noreply.github.com>
2024-02-20 12:05:08 +0300

    Update HowSey.java

---
+++ HowSey.java
@@ -0,0 +1,11 @@
+package Work3;
+
+public class HowSey {
+    @SuppressWarnings("unused")
+    private String formality;
+
+    public void setFormality(String formality) {
+        this.formality = formality;
+    }
+    
+}
